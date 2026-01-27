6c776c60da87467ab51ddd81446836711ccf1098
chethan kumar m s <chethan@zotanextech.com>
2026-01-27 10:58:50 +0530

    some of apis are removed

--- src/routes/subRoutes/productRoutes.ts
+++ src/routes/subRoutes/productRoutes.ts
@@ -9,11 +9,4 @@ Routes.get('/api/product/get-by-id', authService.authenticate, Controller.getPro
 Routes.get('/api/product/list', authService.authenticate, Controller.listProducts);
 Routes.get('/api/product/drop-down', authService.authenticate, Controller.productsDropDown);
 
-Routes.post('/api/product-pcf/add', authService.authenticate, Controller.createProductPCF);
-Routes.post('/api/product-pcf/update', authService.authenticate, Controller.updateProductPCF);
-Routes.get('/api/product-pcf/get-by-id', authService.authenticate, Controller.getProductPCFById);
-Routes.post('/api/product-pcf/delete-raw-material-component', authService.authenticate, Controller.deleteRawMaterialComponents);
-Routes.post('/api/product-pcf/delete-transportation', authService.authenticate, Controller.deleteTransportation);
-Routes.post('/api/product-pcf/delete-packaging', authService.authenticate, Controller.deletePackaging);
-
 export default Routes;
