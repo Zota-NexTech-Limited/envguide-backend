c8d12a2b85eec210c88706a5a7bb296f49742fa9
chethan kumar m s <chethan@zotanextech.com>
2026-02-02 16:33:45 +0530

    dashboard spi files created

---
+++ src/routes/subRoutes/dashboardRoutes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import * as Controller from '../../controller/dashboardController';
+import * as  authService from '../../middleware/authService'
+const Routes = Router();
+
+export default Routes;
